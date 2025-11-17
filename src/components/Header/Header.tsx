83726d9d1ca4d886c0ed907c20620305ccadc715
ValeriaMakarevich <lsmolyanova517@gmail.com>
2025-11-17 20:08:30 +0300

    Update the page title rendering function in the header and update the styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,27 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import styles from "./Header.module.css";
+
 
 
 
 function Header() {
 const location = useLocation();
+const {year}= useParams()
 console.log(location)
 
 const getPageTitle = (path: string) => {
   if (path === "/") return "Home";
   if (path === "/timeline") return "Timeline";
-  if (path === "/eras") return "Era";
+  if (path.startsWith("/eras/")) return year ;
   if (path === "/gallery") return "Gallery";
   if (path === "/about") return "About";
   return "Page Not Found";
 };
 
   return (
-    <div className={styles.container}>{getPageTitle(location.pathname)}</div>
+    <div className={styles.container}>
+      <div className={styles.text}>{getPageTitle(location.pathname)}</div>
+    </div>
   );
 }
 
